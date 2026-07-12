202131218c3a4c208ce0d74e4f040ee5c605597b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "postgresql_flexible_server_configurations" {
-  description = "All postgresql_flexible_server_configuration resources"
-  value       = azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations
-}
 output "postgresql_flexible_server_configurations_name" {
-  description = "List of name values across all postgresql_flexible_server_configurations"
-  value       = [for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : v.name]
+  description = "Map of name values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.name }
 }
 output "postgresql_flexible_server_configurations_server_id" {
-  description = "List of server_id values across all postgresql_flexible_server_configurations"
-  value       = [for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : v.server_id]
+  description = "Map of server_id values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.server_id }
 }
 output "postgresql_flexible_server_configurations_value" {
-  description = "List of value values across all postgresql_flexible_server_configurations"
-  value       = [for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : v.value]
+  description = "Map of value values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.value }
 }
 
