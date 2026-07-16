89ac7089a887f4f19019f8437ad700b6faa09a64
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "postgresql_flexible_server_configurations_id" {
   description = "Map of id values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
-  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.id }
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "postgresql_flexible_server_configurations_name" {
   description = "Map of name values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
-  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.name }
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "postgresql_flexible_server_configurations_server_id" {
   description = "Map of server_id values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
-  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.server_id }
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.server_id if v.server_id != null && length(v.server_id) > 0 }
 }
 output "postgresql_flexible_server_configurations_value" {
   description = "Map of value values across all postgresql_flexible_server_configurations, keyed the same as var.postgresql_flexible_server_configurations"
-  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.value }
+  value       = { for k, v in azurerm_postgresql_flexible_server_configuration.postgresql_flexible_server_configurations : k => v.value if v.value != null && length(v.value) > 0 }
 }
 
